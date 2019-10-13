ce967932d3abf29b13ec6eb98b0717f540e9eaf4
Jigar Patel <pateljj04@gmail.com>
2019-10-13 14:35:39 -0400

    Added Turn code

--- explorerrobot.py
+++ explorerrobot.py
@@ -15,10 +15,15 @@ def backwards():
     sleep(1)
     explorerhat.motor.one.stop()
     explorerhat.motor.two.stop()
-    
+ 
+def turn():
+    explorerhat.motor.one.forward(100)
+    explorerhat.motor.two.backward(100)
+
 app = App("Robot controller")
 
 drive = PushButton(app, forwards, text="Forwards")
 reverse = PushButton(app, backwards, text="Backwards")
+turns = PushButton(app, turn(), text="Turn")
 
 app.display
