f29f3018a367012f403d0c4fa75c908d39910168
Jigar Patel <pateljj04@gmail.com>
2020-11-05 11:57:51 -0500

    Added error correction

--- drone.py
+++ drone.py
@@ -1,23 +1,30 @@
 from pyparrot.Minidrone import Mambo
-
+import sys
 # Change this to the address of the mambo
 mamboAddr = "D0:3A:4B:91:E6:24"
+# Set True/False for the wifi depending on if you are using the wifi or the BLE to
 
 mambo = Mambo(mamboAddr, use_wifi=False)
 print("trying to connect")
-
 success = mambo.connect(num_retries=3)
-
 print("connected: %s" % success)
 
-if (success):
-    # get the state information
-    print("sleeping")
-    mambo.smart_sleep(2)
-    print("taking off!")
-    mambo.safe_takeoff(5)
-    print("landing")
-    mambo.safe_land(5)
-    mambo.smart_sleep(5)
-    print("disconnect")
+try:
+    if (success):
+         # get the state information
+     print("sleeping")
+     mambo.smart_sleep(2)
+     print("taking off!")
+     mambo.safe_takeoff(5)
+     print("turning")
+     mambo.turn_degree(90)
+     print("landing")
+     mambo.safe_land(5)
+     mambo.smart_sleep(5)
+     print("disconnect")
+     mambo.disconnect()
+except:
+    print("Error: Uh oh check your code!.\n")
+    mambo.safe_land(1)
     mambo.disconnect()
+    sys.exit()
